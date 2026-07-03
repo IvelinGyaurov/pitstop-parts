cbf2f7fd004cd94d33ecf0f8d5a49960b5292b5f
IvelinGyaurov <massa9424@gmail.com>
2026-07-03 23:38:39 +0300

    feat: initial parts microservice

---
+++ src/main/java/com/pitstop/pitstop_parts/PitstopPartsApplication.java
@@ -0,0 +1,13 @@
+package com.pitstop.pitstop_parts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitstopPartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitstopPartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitstop/pitstop_parts/PitstopPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitstop.pitstop_parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitstopPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
